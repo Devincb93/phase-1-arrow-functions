a0c0b7bd4b0271f0372a12023fec2d2eee50d9d1
Devin <devincb93@gmail.com>
2023-07-05 18:32:12 -0700

    Arrow Functions Lab

---
+++ index.js
@@ -0,0 +1,3 @@
+const divide = (paramater1, paramater2) => paramater1 / paramater2
+const square = (paramater1) => paramater1 * paramater1
+const add = (parameter1, paramater2) => parameter1 + paramater2
